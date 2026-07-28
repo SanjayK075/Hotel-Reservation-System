c2ed20d27bb10ccc00c0392d00cc3117876c92cb
Sanjay <sanjayk17905@gmail.com>
2026-07-28 19:57:42 +0530

    Dockerize Hotel Reservation System

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "postgresql://hoteluser:hotelpass@localhost:5432/hotel_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://hoteluser:hotelpass@localhost:5432/hotel_db"
+)
 
 engine = create_engine(DATABASE_URL)
 
